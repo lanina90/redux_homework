16480fa81c805c737832d3a00f0466f85f4956a9
Kateryna Laina <boba4ok@gmail.com>
2023-05-10 15:28:46 +0300

    ListRedux.jsx refactor to functional component

--- src/components/list/ListRedux.jsx
+++ src/components/list/ListRedux.jsx
@@ -1,52 +1,89 @@
 import React, {Component} from 'react';
-import {connect} from "react-redux";
+import {connect, useDispatch, useSelector} from "react-redux";
 import {inputAction, listAction} from "./listAction";
 
 
-class ListRedux extends Component {
-  inputOnChangeHandler = (e) => {
-    this.props.setInputValue(e.target.value)
+const ListRedux = () => {
+
+  const dispatch = useDispatch()
+  const listArr = useSelector(state => state.listArr)
+  const inputValue = useSelector(state => state.inputValue)
+
+  const inputOnChangeHandler = (e) => {
+    dispatch(inputAction(e.target.value))
   }
 
-  handleButtonClick = () => {
-    this.props.addItem(this.props.inputValue);
-    this.props.setInputValue('');
-
+  const handleButtonClick = () => {
+    dispatch(listAction(inputValue))
+    dispatch(inputAction(''))
   };
 
-  render() {
-    const {listArr} = this.props
 
-    const list = listArr.map((el, i)=> {
-      return <p key={el}>{i} - {el}</p>
+  const list = listArr.map((el, i) => {
+    return <p key={el}>{i} - {el}</p>
+  })
 
-    })
+  return (
+    <div className='list-one'>
 
-    return (
-      <div className='list-one'>
-        <h1>List</h1>
-        {list}
-        <input
-          type="text"
-          value={this.props.inputValue}
-          onChange={(event) => this.inputOnChangeHandler(event)}
-        />
-        <button onClick={this.handleButtonClick}>ADD TO LIST</button>
-      </div>
-    );
-  }
-}
-const mapStateToProps = (state) => {
-  return {
-    listArr: state.listArr, // здесь больше ничего не нужно изменять
-    inputValue: state.inputValue,
-  };
+      <h1>List</h1>
+      {list}
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(event) => inputOnChangeHandler}
+      />
+      <button onClick={handleButtonClick}>ADD TO LIST</button>
+    </div>
+  );
 };
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    setInputValue: (value) => dispatch(inputAction(value)),
-    addItem: (item) => dispatch(listAction(item)),
-  };
-};
-export default connect(mapStateToProps, mapDispatchToProps)(ListRedux);
+export default ListRedux;
+
+// class ListRedux extends Component {
+//   inputOnChangeHandler = (e) => {
+//     this.props.setInputValue(e.target.value)
+//   }
+//
+//   handleButtonClick = () => {
+//     this.props.addItem(this.props.inputValue);
+//     this.props.setInputValue('');
+//
+//   };
+//
+//   render() {
+//     const {listArr} = this.props
+//
+//     const list = listArr.map((el, i)=> {
+//       return <p key={el}>{i} - {el}</p>
+//
+//     })
+//
+//     return (
+//       <div className='list-one'>
+//         <h1>List</h1>
+//         {list}
+//         <input
+//           type="text"
+//           value={this.props.inputValue}
+//           onChange={(event) => this.inputOnChangeHandler(event)}
+//         />
+//         <button onClick={this.handleButtonClick}>ADD TO LIST</button>
+//       </div>
+//     );
+//   }
+// }
+// const mapStateToProps = (state) => {
+//   return {
+//     listArr: state.listArr,
+//     inputValue: state.inputValue,
+//   };
+// };
+//
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     setInputValue: (value) => dispatch(inputAction(value)),
+//     addItem: (item) => dispatch(listAction(item)),
+//   };
+// };
+// export default connect(mapStateToProps, mapDispatchToProps)(ListRedux);
