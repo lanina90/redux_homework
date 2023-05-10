182fc7afef88f954620c004760649788371e5cf8
Kateryna Laina <boba4ok@gmail.com>
2023-05-10 15:18:58 +0300

    CounterRedux.jsx refactor to functional component

--- src/components/counter/CounterRedux.jsx
+++ src/components/counter/CounterRedux.jsx
@@ -1,26 +1,46 @@
 import React, {Component} from 'react';
 import {decActionCreator, incActionCreator, randActionCreator} from "./counterAction";
-import {connect} from "react-redux";
+import {connect, useDispatch, useSelector} from "react-redux";
 
-class CounterRedux extends Component {
 
-  render() {
-    const {count, dispatch} = this.props
+const CounterRedux = () => {
 
-    return (
-      <div>
-        <h1>Counter</h1>
-        <h2>{count}</h2>
-        <button onClick={()=>dispatch(incActionCreator())}>INC</button>
-        <button onClick={()=>dispatch(decActionCreator())}>DEC</button>
-        <button onClick={()=>dispatch(randActionCreator(Math.floor(Math.random()*10))) }>RANDOM</button>
-      </div>
-    );
-  }
-}
-const mapStateToProps = (state) => {
-  return {
-    count: state.count
-  }
-}
-export default connect(mapStateToProps)(CounterRedux);
+  const dispatch = useDispatch()
+  const count = useSelector(state => state.count)
+
+  return (
+    <div>
+      <h1>Counter</h1>
+      <h2>{count}</h2>
+      <button onClick={() => dispatch(incActionCreator())}>INC</button>
+      <button onClick={() => dispatch(decActionCreator())}>DEC</button>
+      <button onClick={() => dispatch(randActionCreator(Math.floor(Math.random() * 10)))}>RANDOM</button>
+    </div>
+  );
+};
+
+export default CounterRedux;
+
+
+// class CounterRedux extends Component {
+//
+//   render() {
+//     const {count, dispatch} = this.props
+//
+//     return (
+//       <div>
+//         <h1>Counter</h1>
+//         <h2>{count}</h2>
+//         <button onClick={()=>dispatch(incActionCreator())}>INC</button>
+//         <button onClick={()=>dispatch(decActionCreator())}>DEC</button>
+//         <button onClick={()=>dispatch(randActionCreator(Math.floor(Math.random()*10))) }>RANDOM</button>
+//       </div>
+//     );
+//   }
+// }
+// const mapStateToProps = (state) => {
+//   return {
+//     count: state.count
+//   }
+// }
+// export default connect(mapStateToProps)(CounterRedux);
