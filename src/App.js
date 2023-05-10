ed765c9589705f5eb08e07c39cd0b44c04a775a8
Kateryna Laina <boba4ok@gmail.com>
2023-05-10 15:38:01 +0300

    App.js refactor to functional component

--- src/App.js
+++ src/App.js
@@ -3,31 +3,58 @@ import CounterRedux from "./components/counter/CounterRedux";
 import {resetActionCreator} from "./components/counter/counterAction";
 
 import React, {Component} from 'react';
-import {connect} from "react-redux";
+import {connect, useDispatch} from "react-redux";
 import ListRedux from "./components/list/ListRedux";
 import ToDoRedux from "./components/todolist/ToDoRedux";
 import ToDoAsyncRedux from "./components/asyncList/ToDoAsyncRedux";
 
 
-class App extends Component {
-  render() {
-    const {dispatch} = this.props
-    return (
-      <div>
-        <div className='counter'>
-          <button onClick={() => dispatch(resetActionCreator())}>RESET COUNTER FROM APP</button>
-          <CounterRedux/>
-        </div>
 
-        <ListRedux/>
 
-        <ToDoRedux/>
+const App = () => {
+  const dispatch = useDispatch()
 
-        <ToDoAsyncRedux/>
-
+  return (
+    <div>
+      <div className='counter'>
+        <button onClick={() => dispatch(resetActionCreator())}>RESET COUNTER FROM APP</button>
+        <CounterRedux/>
       </div>
-    );
-  }
-}
 
-export default connect()(App);
+      <ListRedux/>
+
+      <ToDoRedux/>
+
+      <ToDoAsyncRedux/>
+
+    </div>
+  );
+};
+
+export default App;
+
+
+
+
+// class App extends Component {
+//   render() {
+//     const {dispatch} = this.props
+//     return (
+//       <div>
+//         <div className='counter'>
+//           <button onClick={() => dispatch(resetActionCreator())}>RESET COUNTER FROM APP</button>
+//           <CounterRedux/>
+//         </div>
+//
+//         <ListRedux/>
+//
+//         <ToDoRedux/>
+//
+//         <ToDoAsyncRedux/>
+//
+//       </div>
+//     );
+//   }
+// }
+//
+// export default connect()(App);
